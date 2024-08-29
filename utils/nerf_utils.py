534db55a684f5497e21bdbbe9f0501bbfd874be5
Baptiste <brument.bcb@gmail.com>
2024-08-29 16:45:03 +0200

    oui

--- utils/nerf_utils.py
+++ utils/nerf_utils.py
@@ -9,24 +9,29 @@ from utils.utils import load_image, save_image, get_view_parameters, _cv_to_gl, 
 def write_nerf_data(views_data, intrinsics_data, poses_data, output_path, bit_depth=16):
     """
     Write NeRF .json file.
+    Inspired by
+    """
+    return 0
+
+def write_neus2_data(views_data, intrinsics_data, poses_data, output_path, bit_depth=16, copy_images=True):
+    """
+    Write NeuS2 .json file.
+    Inspired by https://github.com/19reborn/NeuS2/blob/main/tools/data_format_from_neus.py
     """
 
     # Create output directory
-    images_dir = "images"
+    images_dir = "image_masked"
     output_images_path = os.path.join(output_path, images_dir)
-    os.makedirs(output_images_path, exist_ok=True)
 
     # Create output object
-    output = {
-        "w": 0,
-        "h": 0,
+    out = {
         "aabb_scale": 1.0,
-        # "scale": 0.5,
-        # "offset": [ # neus: [-1,1] ngp[0,1]
-        #     0.5,
-        #     0.5,
-        #     0.5
-        # ],
+        "scale": 0.5,
+        "offset": [ # neus: [-1,1] ngp[0,1]
+            0.5,
+            0.5,
+            0.5
+        ],
         "from_na": True,
         "frames": []
     }
@@ -34,57 +39,52 @@ def write_nerf_data(views_data, intrinsics_data, poses_data, output_path, bit_de
     # Iterate over the views
     for i, (view_id, view_data) in enumerate(views_data.items()):
 
-        # Update width and height
-        if i == 0:
-            output["w"] = view_data["width"]
-            output["h"] = view_data["height"]
-
         # Load image
         image_path = view_data["path"]
         undisto_image_path = view_data.get("undistortedImagePath", None)
         if undisto_image_path is not None:
             image_path = undisto_image_path    
-        image = load_image(image_path)
+        image_name = os.path.basename(image_path)
 
-        # Load mask and save image
-        copy_image_bol = False
-        mask_path = view_data.get("maskPath", None)
-        if mask_path is not None:
-            mask = (load_image(mask_path)[:,:,0] > 0.5).astype(np.float32)
-        elif image.shape[-1] == 4:
-            shutil.copyfile(image_path, os.path.join(output_images_path, f"{i:08d}.png"))
-            copy_image_bol = True
-        else:
-            mask = np.ones((image.shape[0], image.shape[1]), dtype=np.float32)
+        if copy_images:
+            image = load_image(image_path)
+            os.makedirs(output_images_path, exist_ok=True)
+            mask_path = view_data.get("maskPath", None)
+            if mask_path is not None:
+                mask = (load_image(mask_path)[:,:,0] > 0.5).astype(np.float32)
+            else:
+                mask = np.ones((image.shape[0], image.shape[1]), dtype=np.float32)
 
-        # Save image
-        if not copy_image_bol:
             if image.shape[-1] == 3:
                 image = np.concatenate([image, mask[...,np.newaxis]], axis=-1)
             elif image.shape[-1] == 4:
                 image[:,:,-1] = (image[:,:,-1] > 0.5).astype(np.float32) * mask
-            save_image(image, os.path.join(output_images_path, f"{i:08d}.png"), bit_depth=bit_depth)
-
+            save_image(image, os.path.join(output_images_path, image_name), bit_depth=bit_depth)
+        else:
+            images_dir = os.path.relpath(os.path.dirname(image_path), output_path)
+        
         # Get view parameters
-        K, Rt_c2w, _ = get_view_parameters(view_data, intrinsics_data, poses_data)
+        K, c2w_gl, _ = get_view_parameters(view_data, intrinsics_data, poses_data)
 
         # Add frame to output
-        one_frame = {}
-        one_frame["file_path"] = os.path.join(images_dir, f"{i:08d}.png")
-        one_frame["transform_matrix"] = Rt_c2w.tolist()
-        one_frame["intrinsic_matrix"] = K.tolist()
-        output["frames"].append(one_frame)
+        frame = {}
+        frame["file_path"] = os.path.join(images_dir, image_name)
+        frame["transform_matrix"] = c2w_gl.tolist()
+        frame["intrinsic_matrix"] = K.tolist()
+        out["frames"].append(frame)
 
-    # Write "transform.json" (handle é, è, à, etc.)
-    with open(os.path.join(output_path, "transform.json"), "w", encoding="utf-8") as f:
-        json.dump(output, f, indent=4)
+    # Get width and height
+    w, h = float(view_data["width"]), float(view_data["height"])
+    out.update({
+        "w": int(w),
+        "h": int(h),
+    })
 
-def write_neus2_data(views_data, intrinsics_data, poses_data, output_path, bit_depth=16):
-    """
-    Write NeuS2 .json file.
-    Inspired by https://github.com/19reborn/NeuS2/blob/main/tools/data_format_from_neus.py
-    """
-
+    # Write data to json file
+    file_path = os.path.join(output_path, 'transforms.json')
+    with open(file_path, "w", encoding="utf-8") as outputfile:
+        json.dump(out, outputfile, indent=4)
+    print('Writing data to json file: ', file_path)
 
 
 def write_neuralangelo_data(views_data, intrinsics_data, poses_data, output_path, bit_depth=16, copy_images=True):
@@ -182,7 +182,8 @@ def write_neuralangelo_data(views_data, intrinsics_data, poses_data, output_path
         "sphere_radius": 1.,
     })
 
-    file_path = os.path.join(output_path, 'transforms_neuralangelo.json')
+    # Write data to json file
+    file_path = os.path.join(output_path, 'transforms.json')
     with open(file_path, "w", encoding="utf-8") as outputfile:
         json.dump(out, outputfile, indent=4)
     print('Writing data to json file: ', file_path)
